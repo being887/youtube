b72a4f4be70672ef851cd173142c2699bc7dbce1
being887 <59926850+being887@users.noreply.github.com>
2023-02-25 11:03:41 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,44 +1,95 @@
 const express = require('express');
-const { spawn } = require('child_process');
 const ytdl = require('ytdl-core');
+
+
 const fs = require('fs');
-const port =process.env.PORT || 3000
+const { format } = require('path');
+
+const port = process.env.PORT || 3000;
 
 const app = express();
 app.use(express.json());
-app.use(express.urlencoded({extended:false}));
-app.set("view engine","hbs")
+app.use(express.urlencoded({ extended: false }));
+app.set("view engine", "hbs");
 
-app.get("/",(req,res)=>{
-  res.render("index")
-})
-app.get('/health', (req, res) => {
-  res.status(200).send('200 OK');
+app.get("/", (req, res) => {
+  res.render("index");
 });
 
-app.post('/download', async (req, res) => {
+app.get("/health", (req, res) => {
+  res.status(200).send("200 OK");
+});
+
+app.post('/video', async (req, res) => {
   const videoUrl = req.body.url;
-  const format = req.body.format;
-  const quality = req.body.quality;
-  const filter = (format && quality) ? `format=${format}+${quality}` : 'audioandvideo';
+  const info = await ytdl.getBasicInfo(videoUrl);
 
-  try {
-    const info = await ytdl.getInfo(videoUrl);
-    const videoTitle = info.videoDetails.title.replace(/[^\w\s]/gi, '');
+  const format1 = info.formats.filter((format) => (format.itag <= 150 && format.itag >= 100));
   
-    res.setHeader('Content-disposition',`attachment; filename="${videoTitle}.mp4"`);
-    const youtubeDl = ytdl(videoUrl, { filter });
-    youtubeDl.pipe(res);
-  
-  } catch (error) {
-    console.log(error);
-    res.status(500).send('Error downloading video');
-  }
+  const videoData = format1.map((format) => {
+    return {
+      thumbnail: info.videoDetails.thumbnails[0].url,
+      title: info.videoDetails.title,
+      qualityLabel: format.qualityLabel,
+      url: videoUrl
+    }
+  });
+
+  res.render("index", { videoData });
 });
 
+app.post('/download1', async (req, res) => {
+  const videoUrl = req.body.url;
+  const quality = req.body.quality;
+  const info = await ytdl.getInfo(videoUrl);
+  const videoTitle = info.videoDetails.title.replace(/[^\w\s]/gi, '');
+  res.setHeader('Content-disposition', `attachment; filename="${videoTitle}".mp4`);
+  const options={
+    filter:format=>format.qualityLabel===quality,
+    
+  }
+ 
+  const video=ytdl(videoUrl,options);
 
 
 
+     video.pipe(res);
+});
+app.post('/download2',async(req,res)=>{
+  const quality = req.body.quality;
+  const videoUrl=req.body.url;
+  const info = await ytdl.getInfo(videoUrl);
+  const videoTitle = info.videoDetails.title.replace(/[^\w\s]/gi, '');
+  res.setHeader('Content-disposition', `attachment; filename="${videoTitle}".mp4`);
+  const options1={
+    filter:'videoandaudio'
+  }
+  const video1=ytdl(videoUrl,options1);
+  video1.pipe(res);
+})
+
+app.post('/download3',async(req,res)=>{
+  
+  const videoUrl=req.body.url;
+  const info = await ytdl.getInfo(videoUrl);
+  const videoTitle = info.videoDetails.title.replace(/[^\w\s]/gi, '');
+  res.setHeader('Content-disposition', `attachment; filename="${videoTitle}".mp3`);
+  const options1={
+    filter:'audioonly'
+  }
+  const video1=ytdl(videoUrl,options1);
+  video1.pipe(res);
+})
+app.post('/download4',async(req,res)=>{
+  
+  const videoUrl=req.body.url;
+  const info = await ytdl.getInfo(videoUrl);
+  const videoTitle = info.videoDetails.title.replace(/[^\w\s]/gi, '');
+  const keywords=info.videoDetails.keywords;
+   console.log(keywords);
+   res.render('index', { keywords: keywords });
+
+})
 
 
 app.listen(port, () => {
